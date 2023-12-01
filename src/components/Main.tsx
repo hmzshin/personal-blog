a4471a309152519bf1eb2777e28337acf383fd92
hmzshin <hamzasahin@outlook.com>
2023-12-01 17:00:04 +0300

    create page components

---
+++ src/components/Main.tsx
@@ -0,0 +1,17 @@
+const Main = () => {
+  return (
+    <>
+      <div>
+        <label>
+          <input></input>
+        </label>
+        <label>
+          <input></input>
+        </label>
+      </div>
+      <div></div>
+    </>
+  );
+};
+
+export default Main;
