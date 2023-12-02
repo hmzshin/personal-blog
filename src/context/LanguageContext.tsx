680c8b231d8cc74ef390ec15f5344d57b9aeb201
hmzshin <hamzasahin@outlook.com>
2023-12-02 14:59:00 +0300

    move axios request to app.ts

--- src/context/LanguageContext.tsx
+++ src/context/LanguageContext.tsx
@@ -1,22 +1,9 @@
-import { createContext, useState, useEffect } from "react";
-import { languages } from "../data.ts";
-import axios from "axios";
+import { createContext, useState } from "react";
 
 export const LanguageContextObject = createContext({});
 
 const LanguageContextProvider = ({ children }: any) => {
-  const [language, setLanguage] = useState(languages.english);
-  useEffect(() => {
-    axios
-      .post("https://reqres.in/api/users", languages)
-      .then(function (response) {
-        console.log(response.data);
-        setLanguage(response.data.turkish);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
+  const [language, setLanguage] = useState();
 
   function changeLanguage(value: any) {
     setLanguage(value);
