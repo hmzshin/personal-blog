f134c9481f259345b4d8f1cc5c285ba2ddc8de96
hmzshin <hamzasahin@outlook.com>
2023-12-02 14:56:25 +0300

    load tata from api when  app.js renders

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import "./App.css";
 import Main from "./components/Main";
+import Profile from "./components/Profile";
 import Skills from "./components/Skills";
+import { useContext, useEffect } from "react";
+import { LanguageContextObject } from "./context/LanguageContext";
+import axios from "axios";
+import { languages } from "./data";
 
 function App() {
+  const { language, changeLanguage }: any = useContext(LanguageContextObject);
+  useEffect(() => {
+    axios
+      .post("https://reqres.in/api/users", languages)
+      .then(function (response) {
+        changeLanguage(response.data.english);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
   return (
     <>
-      <Main />
-      <Skills />
+      {language ? (
+        <>
+          <Main />
+          <Skills />
+          <Profile />
+        </>
+      ) : (
+        <div>yükleniyor</div>
+      )}
     </>
   );
 }
